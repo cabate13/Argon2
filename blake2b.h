0f4405da67a8987a628fbd22b186227916f2bd6f
samueleAndreoli <samuele.andreoli@yahoo.it>
2017-01-03 14:47:09 +0100

    Aggiunta interfaccia minima per la libreria con blake2b Modificate alcune funzioni nella compressione Per compilare guarda nel readme

---
+++ blake2b.h
@@ -0,0 +1,32 @@
+// Blake2b implementation
+//
+//      C implementation of the blake2b multi-length hash function
+//      and its round function.
+//      
+//      Credits to:  Jean-Philippe Aumasson, Samuel Neves, Zooko Wilcox-O'Hearn, and Christian Winnerlein
+//
+//  Notation and function names are used accordingly to this independent IETF submission:
+//  https://tools.ietf.org/html/rfc7693
+//
+//  Credits to: M-J. Saarinen, Ed.
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if !defined BLAKE2B
+
+#define BLAKE2B
+
+// Unsigned integer with 128 bits precision
+typedef struct {
+        uint64_t left;
+        uint64_t right;
+} uint128_t;
+
+// Blake2b hash function function
+void blake2b( void* digest, size_t nn, void* data, size_t ll, void* key, size_t kk);
+
+#endif
