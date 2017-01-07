018b8cf8de37b734a0c77f487064bd763ef9d1a7
carmine.abate@studenti.unitn.it <carmine.abate@studenti.unitn.it>
2017-01-07 17:20:55 +0100

    Inserted file of Utility Function for CompressionFunctionG

--- Argon2_compression_test.c
+++ Argon2_compression_test.c
@@ -2,66 +2,36 @@
 #include <stdint.h>
 #include "Argon2_compression.h"
 #include "blake2b.h"
+//#include "SomeUtilityFunctions.h"
+
+
+
 
 int main(void)
 {
-
-        /*      
-        uint128_t S[8];
-
-        for (int i = 0; i < 8; ++i)
-        {
-                S[i].left = 4294967297 +i;
-                printf("%lu\n", S[i].left);
-                S[i].right = 4294967299 + 10*i;
-                printf("%lu\n", S[i].right);
-        }
-
-        printf("\n\n");
-        P((uint128_t*) &S);
-
-        for (int i = 0; i < 8; ++i)
-        {
-                printf("%lu\n", S[i].left);
-                printf("%lu\n", S[i].right);
-        }       
-        */
 
         uint128_t X[64];
         uint128_t Y[64];
 
         for (int i = 0; i < 64; ++i)
         {
-                X[i].left = 1;
-                Y[i].right = 0;
-                X[i].right = 0;
-                Y[i].left = 0;
+            X[i].left =  4294967297 +i;
+            Y[i].left =  429496729 + 2*i;
+            X[i].right = Y[i].left;
+            Y[i].right = X[i].left;
         }
 
-        
-        uint128_t T[64];
+        uint128_t* res;
+        res = (uint128_t*) malloc(16*64);
 
-        for (int i = 0; i < 64; ++i)
-        {
-                
-                T[i].left = 0;
-                T[i].right = 0;
+        CompressionFunctionG(X,Y,res);
 
-        }
+        printf("res: \n");
+        for(int i = 0;i<64;i++)
+            printf("%016lX | %016lX \n", res[i].right, res[i].left);
+        printf("\n");
+        free(res);
 
-        printf("FINE\n");
-
-        CompressionFunctionG(X,Y,T);
-
-        
-        for (int i = 0; i < 64; ++i)
-        {
-                
-                uint64_t d = (*(T+i)).left;
-                uint64_t t = (*(T+i)).right;
-
-                printf("%016llX | %016llX\n", t, d);
-        }
-        
         return 0;
 }
+
